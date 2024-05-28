d42c17ab930ce7a9c69e359fafdc6dcd6e8d820f
Chepakada <prashantlamichhane68@gmail.com>
2024-05-27 19:01:22 -0600

    file_upload_successfull

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 
@@ -12,9 +13,17 @@ migrate = Migrate()
 ma = Marshmallow()
 cors = CORS()
 
+
+
+
+
 def create_app():
     """Application-factory pattern"""
     app = Flask(__name__)
+    UPLOAD_FOLDER = "uploads/files"
+    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+    app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+    
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
