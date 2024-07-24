00df4afd12ab0af67d8899126e6ef6d1c901d720
SubWal <subodh333@byui.edu>
2024-07-23 20:21:36 -0600

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from "react";
 import {Header, Footer, InputBox} from "./modules/Components.tsx"
 // import { InputBox } from './modules/Home.tsx';
 
